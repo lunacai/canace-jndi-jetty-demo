e2be20368e6b5d0648cf77faae3e1f7ad9a20699
lunacaishulie.io <lunacaishulie.io>
2020-08-18 16:47:16 +0800

    first commit

---
+++ canace-jetty-jndi-demo/src/main/java/com/example/canacejettyjndidemo/CanaceJettyJndiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.canacejettyjndidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaceJettyJndiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CanaceJettyJndiDemoApplication.class, args);
+    }
+
+}
---
+++ canace-jetty-jndi-demo/src/test/java/com/example/canacejettyjndidemo/CanaceJettyJndiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canacejettyjndidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaceJettyJndiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
